1f332c11140b54f5b54490b7c5a084e7d116dd13
Shabbirali Momin <99755354+nepster440@users.noreply.github.com>
2025-02-20 12:03:53 +0530

    Added created_at field to Task model

--- to_do/admin.py
+++ to_do/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task) # Task model ko admin panel me register kiya
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at')  # Show in Admin Panel
+
+admin.site.register(Task, TaskAdmin)
 # Register your models here.
